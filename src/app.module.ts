3145608fd54953e73b1c518d6868fad6d4a22058
Harry <tothefullest08@gmail.com>
2023-07-18 23:57:44 +0900

    [Feat] 앱 모듈 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,40 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { LoggerModule } from 'nestjs-pino';
+import config from '@src/common/config/config';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from '@src/user/user.module';
+import { DatabaseModule } from '@src/common/database/database.module';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExceptionFilter } from '@src/common/exception/http-exception.filter';
 
 @Module({
-  imports: [],
+  imports: [
+    LoggerModule.forRoot({
+      pinoHttp: {
+        quietReqLogger: true,
+        autoLogging: true,
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            singleLine: true,
+          },
+        },
+      },
+    }),
+    ConfigModule.forRoot({
+      load: [config],
+      isGlobal: true,
+    }),
+    UserModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
